8a7164bbdede145a259e66925c48b7139266aa2e
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 10:52:34 -0700

    initializes navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">Benjamin Newitt</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style>
+</style>
