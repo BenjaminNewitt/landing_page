78af2a18dee1ba7b55601409505926c265d1c10a
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 13:08:21 -0700

    moves pageTop to component

---
+++ src/components/PageTop.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page row">
+    <div class="col-12 text-center splash-background">
+      <!-- TODO align content of col with center of background image -->
+      <h1 class="text-light pt-5">Full Stack Software Developer</h1>
+      <button class="btn btn-success">
+        <h2 class="text-light">Learn More</h2>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Page"
+};
+</script>
+
+<style scoped>
+.page {
+  height: 80vh;
+}
+.splash-background {
+  background-image: url(~@/assets/splash.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,24 @@
 <template>
   <div class="home container-fluid">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <div class="row">
-      <div class="col-12 text-center splash-background">
-        <!-- TODO align content of col with center of background image -->
-        <h1 class="text-dark">Full Stack Software Developer</h1>
-        <button class="btn btn-success">
-          <h2>Learn More</h2>
-        </button>
-      </div>
-    </div>
+    <page-top />
     <about />
     <contact />
   </div>
 </template>
 
 <script>
+import PageTop from "@/components/PageTop";
 import About from "@/components/About";
 import Contact from "@/components/ContactMe";
 export default {
   name: "Home",
   components: {
+    PageTop,
     About,
     Contact
   }
 };
 </script>
-<style scoped>
-.splash-background {
-  background-image: url(~@/assets/splash.jpg);
-  background-repeat: no-repeat;
-  background-size: 100% auto;
-  height: 100%;
-}
+<style>
 </style>
