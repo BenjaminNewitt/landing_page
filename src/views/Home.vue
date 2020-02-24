ca7d0a1556f831550002fd05fb01f38c9fa7062a
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 10:39:08 -0700

    installs bootstrap, comments out img

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Hello World</h1>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <h1 class="text-center">Hello World</h1>
   </div>
 </template>
