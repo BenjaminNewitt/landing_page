b4496f789b0336a4ad582d94cd7aa0b14a759460
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 11:27:24 -0700

    adds background image to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="home">
+  <div class="home container-fluid">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <h1 class="text-center">Full Stack Software Developer</h1>
+    <div class="row">
+      <div class="col-12 splash-background">
+        <h1 class="text-center">Full Stack Software Developer</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.splash-background {
+  background-image: url(~@/assets/splash.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  height: 80vh;
+}
+</style>
