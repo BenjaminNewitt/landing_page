97e30a71944dd7dbf6a2781f1ad34c3da1783edd
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 14:33:30 -0700

    draws tools images to page

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,8 +1,39 @@
 <template>
   <div class="tools row">
-    <div class="col-12 bg-dark text-light text-center pt-5">
-      <h2>Languages and Tools</h2>
-      <div class="row"></div>
+    <div class="col-12 bg-light text-dark text-center pt-5 pb-5">
+      <h2 class="pb-3">Languages and Tools</h2>
+      <div class="row justify-content-center">
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/CSS.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/html.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/Vue.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/bootstrap.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/JavaScript.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/mongodb.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/node.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/csharp.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/mySQL.png" alt />
+        </div>
+        <div class="col-6 col-md-2">
+          <img src="~@/assets/git.png" alt />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,5 +44,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+img {
+  max-width: -webkit-fill-available;
+}
 </style>
