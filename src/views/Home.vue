6e1661373011c9f912fc967eb7b9e17a80a48bfa
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 10:55:29 -0700

    changes title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <h1 class="text-center">Hello World</h1>
+    <h1 class="text-center">Full Stack Software Developer</h1>
   </div>
 </template>
