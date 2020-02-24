27daf5a9563504f6f2394adf1f0f2e4bb4b4505a
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 11:35:15 -0700

    adds aboutMe component to home

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h2>About Me</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,32 @@
   <div class="home container-fluid">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <div class="row">
-      <div class="col-12 splash-background">
-        <h1 class="text-center">Full Stack Software Developer</h1>
+      <div class="col-12 text-center splash-background">
+        <!-- TODO align content of col with center of background image -->
+        <h1>Full Stack Software Developer</h1>
+        <button class="btn btn-success">
+          <h2>Learn More</h2>
+        </button>
       </div>
     </div>
+    <about />
   </div>
 </template>
 
+<script>
+import About from "@/components/About";
+export default {
+  name: "Home",
+  components: {
+    About
+  }
+};
+</script>
 <style scoped>
 .splash-background {
   background-image: url(~@/assets/splash.jpg);
   background-repeat: no-repeat;
   background-size: 100% auto;
-  height: 80vh;
+  height: 100vh;
 }
 </style>
