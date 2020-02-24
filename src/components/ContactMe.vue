934c80364e03601d792c43052567e3dc91401b21
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-24 15:07:16 -0700

    adds phone contact to ContactMe

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -4,15 +4,21 @@
       <h2>Contact Me</h2>
       <div class="row d-flex justify-content-center">
         <div class="col-12 col-md-4 text-center">
+          <a href="mailto:benjaminnewitt@gmail.com">
+            <i class="fas fa-envelope fa-5x text-dark"></i>
+            <h4 class="text-dark">benjaminnewitt@gmail.com</h4>
+          </a>
+        </div>
+        <div class="col-12 col-md-4 text-center">
           <a href="https://www.linkedin.com/in/benjamin-newitt/">
             <i class="fab fa-linkedin fa-5x"></i>
             <h4 class="text-dark">LinkedIn</h4>
           </a>
         </div>
         <div class="col-12 col-md-4 text-center">
-          <a href="mailto:benjaminnewitt@gmail.com">
-            <i class="fas fa-envelope fa-5x text-dark"></i>
-            <h4 class="text-dark">benjaminnewitt@gmail.com</h4>
+          <a href="tel:1-916-710-2739">
+            <i class="fas fa-phone-square-alt fa-5x text-dark"></i>
+            <h4 class="text-dark">(916)710-2739</h4>
           </a>
         </div>
       </div>
