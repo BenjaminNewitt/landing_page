f74a2aee50239edb6fa1280b611b22b0fca8e1df
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-25 10:52:20 -0700

    changes aboutMe display

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,27 +7,31 @@
           <hr class="border border-dark" />
         </div>
       </div>
-      <p class="font-italic">
-        If you can fill the unforgiving minute
-        <br />With sixty seconds’ worth of distance run,
-        <br />Yours is the Earth and everything that’s in it.
-      </p>
-      <p>Rudyard Kipling</p>
-      <br />
-      <p>
-        I am a full stack Software developer who enjoys working on a variety of
-        projects. Versatility and adaptability are skills I initially developed
-        working in a wide variety of roles in retail for the past few years, and
-        now that I've transitioned into the exciting industry of software
-        development, I've been able to further reinforce those skills.
-        <br />
-        <br />Coupling those soft skills (alongside focusing on teamwork and
-        collaboration) with industry-specific skills, I'm enjoying entering into
-        a new career field. Working with a handful of languages including C#,
-        JavaScript, as well as frameworks such as Node.js and Vue, has taught me
-        I'm also able to adapt to different virtual environments —and how to
-        appreciate it.
-      </p>
+      <div class="row">
+        <div class="col-8 m-auto">
+          <p class="font-italic">
+            If you can fill the unforgiving minute
+            <br />With sixty seconds’ worth of distance run,
+            <br />Yours is the Earth and everything that’s in it.
+          </p>
+          <p>Rudyard Kipling</p>
+          <br />
+          <p>
+            I am a full stack Software developer who enjoys working on a variety of
+            projects. Versatility and adaptability are skills I initially developed
+            working in a wide variety of roles in retail for the past few years, and
+            now that I've transitioned into the exciting industry of software
+            development, I've been able to further reinforce those skills.
+            <br />
+            <br />Coupling those soft skills (alongside focusing on teamwork and
+            collaboration) with industry-specific skills, I'm enjoying entering into
+            a new career field. Working with a handful of languages including C#,
+            JavaScript, as well as frameworks such as Node.js and Vue, has taught me
+            I'm also able to adapt to different virtual environments —and how to
+            appreciate it.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="github row" id="github">
     <div class="col-12 bg-dark text-light text-center pt-5 pb-5">
-      <h2>Check out my other projects!</h2>
+      <h2>Check out my projects!</h2>
       <div class="row d-flex justify-content-center">
         <div class="col-3 col-md-1">
           <hr class="border border-light" />
