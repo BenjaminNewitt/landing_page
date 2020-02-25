b7f71332f4ff51737326f65c558157a3d9f4c873
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-25 13:21:45 -0700

    adds showcase cards to portfolio

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,7 +1,51 @@
 <template>
   <div class="github row" id="github">
     <div class="col-12 bg-dark text-light text-center pt-5 pb-5">
-      <h2 class="m-0">Check out my projects!</h2>
+      <h2 class="m-0">Showcase</h2>
+      <div class="row d-flex justify-content-center">
+        <div class="col-3 col-md-1">
+          <hr class="border border-light" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-10 m-auto col-sm-6 col-md-4">
+          <div class="card text-dark" style="width: 100%;">
+            <div class="card-body">
+              <h5 class="card-title">Nabnak</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-10 m-auto col-sm-6 col-md-4">
+          <div class="card text-dark" style="width: 100%;">
+            <div class="card-body">
+              <h5 class="card-title">Wild Cards</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-10 m-auto col-sm-6 col-md-4">
+          <div class="card text-dark" style="width: 100%;">
+            <div class="card-body">
+              <h5 class="card-title">Inspire</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2 class="m-0 pt-5">Check out my other projects!</h2>
       <div class="row d-flex justify-content-center">
         <div class="col-3 col-md-1">
           <hr class="border border-light" />
