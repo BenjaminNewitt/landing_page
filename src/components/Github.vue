ab4a6f0c8472995ec965dbbd6dd0807170346256
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-02 12:39:39 -0700

    nabnak coming soon btn and placeholder

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -13,31 +13,6 @@
             class="card text-dark border-dark"
             style="width: 100%; height: 100%"
           >
-            <div class="card-body">
-              <h5 class="card-title">Nabnak</h5>
-              <p class="card-text text-left">
-                Users create their own boards and lists of To-Do's and can write
-                notes for those lists
-              </p>
-              <p class="font-italic card-text text-left">
-                Environment: HTML, CSS, Bootstrap Vue, Node, MongoDB
-              </p>
-            </div>
-            <div class="card-footer">
-              <a
-                href="https://nabnak.herokuapp.com"
-                target="_blank"
-                class="btn btn-dark"
-                >Try it out here</a
-              >
-            </div>
-          </div>
-        </div>
-        <div class="col-10 mx-auto col-sm-6 col-md-4">
-          <div
-            class="card text-dark border-dark"
-            style="width: 100%; height: 100%"
-          >
             <img
               src="~@/assets/wild_cards.png"
               class="card-img-top"
@@ -46,7 +21,12 @@
             <div class="card-body">
               <h5 class="card-title">Wild Cards</h5>
               <p class="card-text text-left">
-                Users build and play card games with friends
+                Wild Cards allows users to make their own basic virtual card
+                game. Users can create cards, which are put together in a deck
+                that has its own set of written rules provided by the user. A
+                user can pick public decks to use that deck with another player.
+                Players have an area to draw and play cards, as well as their
+                own hand.
               </p>
               <p class="font-italic card-text text-left">
                 Environment: HTML, CSS, Bootstrap, Vue, Node, MongoDB
@@ -71,7 +51,11 @@
             <div class="card-body">
               <h5 class="card-title">Keepr</h5>
               <p class="card-text text-left">
-                Site for users to keep and share posts
+                Similar to social media apps such as Pinterest, Keepr allows
+                users to share public keeps on the home page. Registered users
+                may create keeps, as well as create vaults to hold keeps. Keeps
+                may be removed from vaults, and users can delete their vaults
+                and keeps.
               </p>
               <p class="font-italic card-text text-left">
                 Environment: HTML, CSS, Bootstrap, Vue, C#, MySQL, Gearhost
@@ -82,8 +66,42 @@
                 href="https://keepr-v.herokuapp.com"
                 target="_blank"
                 class="btn btn-dark"
-                >Test it Here</a
+                >Try it out</a
               >
+            </div>
+          </div>
+        </div>
+        <div class="col-10 mx-auto col-sm-6 col-md-4">
+          <div
+            class="card text-dark border-dark"
+            style="width: 100%; height: 100%"
+          >
+            <img
+              src="~@/assets/nabnak placeholder.png"
+              class="card-img-top"
+              alt="..."
+            />
+
+            <div class="card-body">
+              <h5 class="card-title">Nabnak</h5>
+              <p class="card-text text-left">
+                Nabnak is a full-stack application that allows users to create
+                boards which hold a variety of lists. Each list can hold tasks
+                with notes to organize user's content, from household chores to
+                preparing for social events.
+              </p>
+              <p class="font-italic card-text text-left">
+                Environment: HTML, CSS, Bootstrap Vue, Node, MongoDB
+              </p>
+            </div>
+            <div class="card-footer">
+              <!-- <a
+                href="https://nabnak.herokuapp.com"
+                target="_blank"
+                class="btn btn-dark"
+                >Try it out here</a
+              > -->
+              <button class="btn btn-dark disabled">Coming soon</button>
             </div>
           </div>
         </div>
