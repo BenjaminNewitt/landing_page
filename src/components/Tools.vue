713063aa40f44bca547d5968c112d9f626213b4a
BenjaminNewitt <benjaminnewitt@gmail.com>
2020-08-18 20:16:15 -0600

    adds links to tools, languages

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -10,52 +10,84 @@
       </div>
       <div class="row justify-content-center">
         <div class="col-6 col-sm-2 pb-2">
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
           <img src="~@/assets/CSS.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML" target="_blank">
           <img src="~@/assets/html.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://vuejs.org/" target="_blank">
           <img src="~@/assets/Vue.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://getbootstrap.com/" target="_blank">
           <img src="~@/assets/bootstrap.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://developer.mozilla.org/en-US/docs/Web/javascript" target="_blank">
           <img src="~@/assets/JavaScript.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://www.mongodb.com/" target="_blank">
           <img src="~@/assets/mongodb.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://nodejs.org/en/" target="_blank">
           <img src="~@/assets/node.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank">
           <img src="~@/assets/csharp.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://www.microsoft.com/en-us/microsoft-365/sharepoint/collaboration" target="_blank">
           <img src="~@/assets/sharepoint.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://azure.microsoft.com/en-us/" target="_blank">
           <img src="~@/assets/azure-logo.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://www.mysql.com/" target="_blank">
           <img src="~@/assets/mySQL.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://code.visualstudio.com/" target="_blank">
           <img src="~@/assets/visual-studio-code.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://visualstudio.microsoft.com/" target="_blank">
           <img src="~@/assets/visual-studio.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://github.com/" target="_blank">
           <img src="~@/assets/github-logo.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://git-scm.com/" target="_blank">
           <img src="~@/assets/git.png" alt />
+          </a>
         </div>
         <div class="col-6 col-sm-2">
+          <a href="https://docs.microsoft.com/en-us/dotnet/core/about" target="_blank">
           <img src="~@/assets/dotnet-core.png" alt />
+          </a>
         </div>
       </div>
     </div>
@@ -75,5 +107,9 @@ img {
 }
 hr {
   border-width: 2px !important;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
