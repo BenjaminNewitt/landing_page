0f3ed97bd1e04bd0c65b3bfaf0cc81627ffa83fd
BenjaminNewitt <benjaminnewitt@gmail.com>
2020-08-20 21:29:32 -0600

    removes unused code

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,12 +15,6 @@
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <!-- <li class="nav-item active">
-          <a class="nav-link" href="#">
-            Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>-->
         <li class="nav-item">
           <a class="nav-link text-light" href="#about">About</a>
         </li>
